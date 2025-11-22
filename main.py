de399dcbe7a8e116d3c14f7583cf07951262a17c
mohamed_hossam <mohamedhossamhiekel@gmail.com>
2025-11-22 16:03:17 +0200

    add jinja

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
-import os
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-
 app.mount("/css", StaticFiles(directory="views/css"), name="css")
 app.mount("/js", StaticFiles(directory="views/js"), name="js")
 app.mount("/style", StaticFiles(directory="views/style"), name="style")
 app.mount("/images", StaticFiles(directory="views/images"), name="images")
 app.mount("/webfonts", StaticFiles(directory="views/webfonts"), name="webfonts")
+templates = Jinja2Templates(directory="views")
 
 @app.get("/")
-async def read_root():
-    return FileResponse("views/Web.html")
+async def read_root(request: Request): 
+    """
+    Renders the main web.html page.
+    """
+    return templates.TemplateResponse(
+        "Web.html", 
+        {
+            "request": request
+        }
+    )
 
 @app.get("/Register")
-async def read_registration():
-    return FileResponse("views/RegistrationForm.html")
-
-
-
+async def read_registration(request: Request): 
+    """
+    Renders the RegistrationForm.html page.
+    """
+    return templates.TemplateResponse(
+        "RegistrationForm.html", 
+        {
+            "request": request
+        }
+    )
