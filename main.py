6ddfb1cb7231b92fa850095627f64d5b942f5e7b
Sami <Ahmedsamiiii2004@gmail.com>
2025-11-22 15:20:01 +0200

    test commit message

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ app = FastAPI()
 
 app.mount("/css", StaticFiles(directory="views/css"), name="css")
 app.mount("/js", StaticFiles(directory="views/js"), name="js")
+app.mount("/test", StaticFiles(directory="views/test"), name="test")
 app.mount("/images", StaticFiles(directory="views/images"), name="images")
 app.mount("/webfonts", StaticFiles(directory="views/webfonts"), name="webfonts")
 
