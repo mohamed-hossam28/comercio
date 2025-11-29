ee9d8755537cbbd3e69a037e830691a4b7dc7909
abdo <amhm77255@gmail.com>
2025-11-29 16:09:34 +0200

    small fix

--- models/products.py
+++ models/products.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String, Float, Text
 from database import Base
 
 class Product(Base):
-    tablename = "products"
+    __tablename__ = "products"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(100), index=True, nullable=False)
